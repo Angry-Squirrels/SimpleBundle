e9a65108ef4f39276787d1be033cb780547d3de5
Thomas BAUDON <baudon.thomas@gmail.com>
2014-09-29 01:03:48 +0200

    some field were not initialized

--- fr/radstar/radengine/components/Transform.hx
+++ fr/radstar/radengine/components/Transform.hx
@@ -11,12 +11,12 @@ import openfl.Lib;
 class Transform 
 {
 
-	public var x : Float = 0;
-	public var y : Float = 0;
+	public var x : Float = 0.0;
+	public var y : Float = 0.0;
 	
-	public var rotation : Float = 0;
+	public var rotation : Float = 0.0;
 	
-	public var scaleX : Float = 1;
-	public var scaleY : Float = 1;
+	public var scaleX : Float = 1.0;
+	public var scaleY : Float = 1.0;
 	
 }
